2c884bc90c5d0ca7689eee3f5e4cc0dce74b1f5d
clarayin129 <clarayin129@gmail.com>
2025-04-05 18:15:31 -0700

    show list of favorites

--- app/(pages)/powders/_components/Example/PowderList.tsx
+++ app/(pages)/powders/_components/Example/PowderList.tsx
@@ -1,20 +1,61 @@
-import React from 'react';
+'use client';
+
+import React, { useState, useEffect } from 'react';
 import styles from './PowderList.module.scss';
 import Powder, { PowderProps } from '../../../_components/Powder/Powder';
+import { getFavorites } from '@utils/favorites';
+import Image from 'next/image';
 
 interface PowderListProps {
   powders: PowderProps[];
 }
 
-export default async function PowderList({ powders }: PowderListProps) {
-  if (!powders || powders.length === 0) {
+export default function PowderList({ powders }: PowderListProps) {
+  const [showFavorites, setShowFavorites] = useState(false);
+  const [visiblePowders, setVisiblePowders] = useState<PowderProps[]>(powders);
+
+  useEffect(() => {
+    if (showFavorites) {
+      const favoriteIds = getFavorites();
+      const filtered = powders.filter((powder) =>
+        favoriteIds.includes(powder.id)
+      );
+      setVisiblePowders(filtered);
+    } else {
+      setVisiblePowders(powders);
+    }
+  }, [showFavorites, powders]);
+
+  if (!visiblePowders || visiblePowders.length === 0) {
     return <div>No powders found</div>;
   }
+
   return (
-    <div className={styles.powderContainer}>
-      {powders.map((powder) => (
-        <Powder key={powder.id} powder={powder} />
-      ))}
+    <div className={styles.body}>
+      <div className={styles.favToggle}>
+        <button
+          onClick={() => setShowFavorites((prev) => !prev)}
+          className={styles.toggleButton}
+        >
+          {showFavorites ? 'show all powders' : 'show favorites'}
+          <Image
+            src={
+              showFavorites
+                ? '/icons/Vectorheart.png'
+                : '/icons/Vectorheart_filled.png'
+            }
+            alt={'favorite icon'}
+            width={20}
+            height={18}
+          />
+        </button>
+      </div>
+
+      <div className={styles.powderContainer}>
+        {visiblePowders.map((powder) => (
+          <Powder key={powder.id} powder={powder} />
+        ))}
+      </div>
     </div>
   );
 }
